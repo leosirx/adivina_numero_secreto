cd0a3547c7859e774aa806ba704577cf89164927
Leonardo Cortes <leosirx@gmail.com>
2023-12-01 20:01:20 -0300

    se corrigio numero secreto

--- adivinanza.js
+++ adivinanza.js
@@ -1,13 +1,17 @@
 const generarNumeroAleatorio = () => {
     return Math.floor(Math.random() * 100) + 1;
 };
+
 const verificarAdivinanza = (numeroSecreto, numeroAdivinado) => {
+
+    numeroAdivinado = parseInt(numeroAdivinado, 10);
+
     if (numeroAdivinado === numeroSecreto) {
-        console.log('¡Felicitaciones! ¡Adivinaste el número secreto!');
+    console.log('¡Felicitaciones! ¡Adivinaste el número secreto!');
     } else if (numeroAdivinado > numeroSecreto) {
-        console.log('El número secreto es menor. ¡Sigue intentando!');
+    console.log('El número secreto es menor. ¡Sigue intentando!');
     } else {
-        console.log('El número secreto es mayor. ¡Sigue intentando!');
+    console.log('El número secreto es mayor. ¡Sigue intentando!');
     }
 };
 module.exports = {
